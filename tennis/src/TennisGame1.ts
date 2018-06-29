8d9f161465f98c605ed8b2cc1f556ceab8449870
ByJC <jeancharles.fauvin@gmail.com>
2018-06-29 12:08:24 +0200

    Cimer Jief <3

--- tennis/src/TennisGame1.ts
+++ tennis/src/TennisGame1.ts
@@ -5,59 +5,39 @@ const scoreMap = {
   1: 'Fifteen',
   2: 'Thirty',
   3: 'Forty'
-}
+};
 
 export class TennisGame1 implements TennisGame {
-  private m_score1: number = 0;
-  private m_score2: number = 0;
-  private player1Name: string;
-  private player2Name: string; 
-
-  constructor(player1Name: string, player2Name: string) {
-    this.player1Name = player1Name;
-    this.player2Name = player2Name;
-  }
+  private scoreP1: number = 0;
+  private scoreP2: number = 0;
 
   wonPoint(playerName: string): void {
-    if (playerName === 'player1')
-      this.m_score1 += 1;
-    else
-      this.m_score2 += 1;
+    playerName === 'player1' ? this.scoreP1 += 1 : this.scoreP2 += 1;
   }
 
-  // map = {}
-  // 0 - Love
-  // 1 - Fifteen
-  // 2 - ..
-
-  // si score1 != score2 && score1 < 4 || score2 <4
-  // return map[score1] - map[score2]
-
   getScore(): string {
-    let score: string = '';
-    let tempScore: number = 0;
-    if (this.m_score1 === this.m_score2) {
-      score = this.getEqualsScore();
-    }
-    else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
-      const minusResult: number = this.m_score1 - this.m_score2;
-      score = Math.abs(minusResult) === 1 ? 'Advantage player' : 'Win for player';
-      score+= this.m_score1 > this.m_score2 ? '1': '2';
-    }
-    else {
-      score = this.getBasicScore()
-    }
+    return this.scoreP1 !== this.scoreP2 && (this.scoreP1 >= 4 || this.scoreP2 >= 4) ?
+      this.getAdvantage() :
+      this.getBasicScore();
+  }
+
+  private getAdvantage(): string {
+    const minusResult: number = this.scoreP1 - this.scoreP2;
+    let score = Math.abs(minusResult) === 1 ? 'Advantage' : 'Win for';
+
+    score += ' player';
+    score += this.scoreP1 > this.scoreP2 ? '1': '2';
+
     return score;
   }
 
-  private getEqualsScore(): string {
-    if (this.m_score1 > 2) {
-      return 'Deuce';
-    }
-    return `${scoreMap[this.m_score1]}-All`;
-  }
-
   private getBasicScore(): string {
-    return `${scoreMap[this.m_score1]}-${scoreMap[this.m_score2]}`;
+    if (this.scoreP1 === this.scoreP2 && this.scoreP1 > 2) {
+      return 'Deuce';
+    } 
+
+    const last = this.scoreP1 === this.scoreP2 ? 'All' : scoreMap[this.scoreP2];
+
+    return `${scoreMap[this.scoreP1]}-${last}`;
   }
 }
