647f71c67aaf0650d550cab2572ae8f735220087
ByJC <jeancharles.fauvin@gmail.com>
2018-08-28 15:43:37 +0200

    [DONE] Robot simulator

--- 6 - RobotSimulator/src/index.ts
+++ 6 - RobotSimulator/src/index.ts
@@ -3,6 +3,30 @@ interface Coordinates {
     y: number
 }
 
+
+const directions = {
+    north: {
+        L: 'west',
+        R: 'east',
+        A: [0, 1]
+    },
+    east: {
+        L: 'north',
+        R: 'south',
+        A: [1, 0]
+    },
+    south: {
+        L: 'east',
+        R: 'west',
+        A: [0, -1]
+    },
+    west: {
+        L: 'south',
+        R: 'north',
+        A: [-1, 0]
+    }
+};
+
 export default class Robot {
     direction: string;
     coordinates: Coordinates;
@@ -11,4 +35,27 @@ export default class Robot {
         this.direction = direction;
         this.coordinates = {x, y};
     } 
+
+    advance() {
+        const [x, y] = directions[this.direction].A;
+
+        this.coordinates.x += x;
+        this.coordinates.y += y;
+    }
+
+    rotate(degree) {
+        this.direction = directions[this.direction][degree];
+    }
+
+    move(command){
+        const autorize = [
+            'L',
+            'R',
+            'A'
+        ]
+        command.split('').filter(el => autorize.includes(el))
+            .forEach(letter => {
+                letter === 'A' ? this.advance() : this.rotate(letter);
+            });
+    }
 }
--- 6 - RobotSimulator/tests/index.spec.ts
+++ 6 - RobotSimulator/tests/index.spec.ts
@@ -10,4 +10,111 @@ describe('Robot Simulator', () => {
       expect(robot.coordinates.x).to.equal(0);
       expect(robot.coordinates.y).to.equal(0);
     });
+
+    it('should be {0,1} north', () => {
+      const robot = new Robot();
+      robot.advance();
+      expect(robot.direction).to.equal('north');
+      expect(robot.coordinates.x).to.equal(0);
+      expect(robot.coordinates.y).to.equal(1);
+    });
+
+    it('should rotate right and be {0,0} east', () => {
+      const robot = new Robot();
+      robot.rotate('R');
+      expect(robot.direction).to.equal('east');
+      expect(robot.coordinates.x).to.equal(0);
+      expect(robot.coordinates.y).to.equal(0);
+    });
+
+
+    it('should rotate left and be {0,0} west', () => {
+      const robot = new Robot();
+      robot.rotate('L');
+      expect(robot.direction).to.equal('west');
+      expect(robot.coordinates.x).to.equal(0);
+      expect(robot.coordinates.y).to.equal(0);
+    });
+
+    it('should rotate left and be {0,0} east when start south', () => {
+      const robot = new Robot(0 ,0, 'south');
+      robot.rotate('L');
+      expect(robot.direction).to.equal('east');
+      expect(robot.coordinates.x).to.equal(0);
+      expect(robot.coordinates.y).to.equal(0);
+    });
+
+    it('should rotate right and be {0,0} south when start east', () => {
+      const robot = new Robot(0 ,0, 'east');
+      robot.rotate('R');
+      expect(robot.direction).to.equal('south');
+      expect(robot.coordinates.x).to.equal(0);
+      expect(robot.coordinates.y).to.equal(0);
+    });
+
+    it('should be {0,-1} when advance south', () => {
+      const robot = new Robot(0 ,0, 'south');
+      robot.advance();
+      expect(robot.direction).to.equal('south');
+      expect(robot.coordinates.x).to.equal(0);
+      expect(robot.coordinates.y).to.equal(-1);
+    });
+
+    it('should be {6, 2} when advance east', () => {
+      const robot = new Robot(5, 2, 'east');
+      robot.advance();
+      expect(robot.direction).to.equal('east');
+      expect(robot.coordinates.x).to.equal(6);
+      expect(robot.coordinates.y).to.equal(2);
+    });
+
+    it('should be {-8, -4} when advance west', () => {
+      const robot = new Robot(-7, -4, 'west');
+      robot.advance();
+      expect(robot.direction).to.equal('west');
+      expect(robot.coordinates.x).to.equal(-8);
+      expect(robot.coordinates.y).to.equal(-4);
+    });
+
+    it('should be {0, 1} when A', () => {
+      const robot = new Robot();
+      robot.move('A');
+      expect(robot.direction).to.equal('north');
+      expect(robot.coordinates.x).to.equal(0);
+      expect(robot.coordinates.y).to.equal(1);
+    });
+
+    it('should be {0, 0} facing west when L', () => {
+      const robot = new Robot();
+      robot.move('L');
+      expect(robot.direction).to.equal('west');
+      expect(robot.coordinates.x).to.equal(0);
+      expect(robot.coordinates.y).to.equal(0);
+    });
+
+    it('should be {0, 1} facing west when AL', () => {
+      const robot = new Robot();
+      robot.move('AL');
+      expect(robot.direction).to.equal('west');
+      expect(robot.coordinates.x).to.equal(0);
+      expect(robot.coordinates.y).to.equal(1);
+    });
+
+    it('should be wrong when Q', () => {
+      const robot = new Robot();
+      robot.move('Q');
+      expect(robot.direction).to.equal('north');
+      expect(robot.coordinates.x).to.equal(0);
+      expect(robot.coordinates.y).to.equal(0);
+    });
+
+    it('should be {-1, 0} facing west when RAAALLADAAA', () => {
+      const robot = new Robot();
+      robot.move('RAAALLADAAA');
+      expect(robot.direction).to.equal('west');
+      expect(robot.coordinates.x).to.equal(-1);
+      expect(robot.coordinates.y).to.equal(0);
+    });
+
+
   });
