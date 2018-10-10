f3d99e8d969f3cf701565d2ee859fb49a0e501f3
Thomas Leveque <thomasleveque@portrad104.tradelab.local>
2018-10-10 16:53:04 +0200

    step 3

--- 7 - ArrayChunk/src/index.ts
+++ 7 - ArrayChunk/src/index.ts
@@ -2,17 +2,24 @@ export default class ArrayChunk {
    constructor() {}
 
    chunk(arr, size) {
+    let i = 0;
 
-       return arr.reduce((acu,number,index) => {  
-       
-        if (acu[(index+1)%size]){
-            acu[(index+1)%size].push(number);
+       return arr.reduce((acu,number,index) => {
+
+        if(arr.length === size){
+            acu[index] = [number];
+            return acu;
         }
 
-        else{
-            acu[(index+1)%size] = [number];  
-        } 
-           return acu; 
+        if (acu[i]){
+            acu[i].push(number);
+        } else {
+            acu[i] = [number]
+        }
+
+        if((index+1)%size === 0) i++;
+
+        return acu; 
         },[]);
    }
    
