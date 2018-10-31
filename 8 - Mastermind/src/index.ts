828ebc0eb6cdbe744a2cd424b8ec6dbe8edbc000
Jean-Franck <jfb@tradelab.fr>
2018-10-31 18:00:03 +0100

    Resolve (almost) mastermind

--- 8 - Mastermind/src/index.ts
+++ 8 - Mastermind/src/index.ts
@@ -1,1 +1,17 @@
-export default class Mastermind {}
+export default class Mastermind {
+  evaluate (secret: string[], proposition: string[]){
+    let wellPlaced: number = 0;
+    let missPlaced: number = 0;
+
+    secret.forEach((element, index) => {
+      if (proposition[index] === element) {
+        wellPlaced++;
+      }
+      else if (proposition.includes(element)) {
+        missPlaced++;
+      }
+    });
+
+    return [wellPlaced, missPlaced];
+  }
+}
--- 8 - Mastermind/tests/index.spec.ts
+++ 8 - Mastermind/tests/index.spec.ts
@@ -4,4 +4,20 @@ import Mastermind from '../src/index';
 
 describe('Mastermind', () => {
   const mastermind = new Mastermind();
+
+  it ('One color well placed', () => {
+    expect(mastermind.evaluate(['blue'],['blue'])).to.deep.equal([1,0])
+  });
+
+  it ('One color not present', () => {
+    expect(mastermind.evaluate(['blue'],['black'])).to.deep.equal([0,0])
+  });
+
+  it ('Two colors misplaced', () => {
+    expect(mastermind.evaluate(['blue','black'],['black','blue'])).to.deep.equal([0,2])
+  });
+
+  it ('One color well placed and two colors miss placed', () => {
+    expect(mastermind.evaluate(['blue','white','blue'], ['white','blue','blue'])).to.deep.equal([1,2])
+  });
 });
