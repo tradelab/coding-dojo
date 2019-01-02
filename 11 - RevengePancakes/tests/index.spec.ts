2d3250af8b6f0c35e67f15631eb7590e82f4d941
ByJC <jeancharles.fauvin@gmail.com>
2019-01-02 11:40:59 +0100

    RevengePancakes

---
+++ 11 - RevengePancakes/tests/index.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import RevengePancakes from '../src/index';
+
+describe('The Revenge of the pancakes', () => {
+    const pancakes = new RevengePancakes();
+
+    it ('', () => {
+    });
+});
