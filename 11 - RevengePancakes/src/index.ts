d14d95fd58c7c32af314b41f496c4adaf08bee28
ByJC <jeancharles.fauvin@gmail.com>
2019-01-04 15:43:19 +0100

    WIP 11

--- 11 - RevengePancakes/src/index.ts
+++ 11 - RevengePancakes/src/index.ts
@@ -1,2 +1,23 @@
 export default class RevengePancakes {
+    flip(pancakes : string[], str = '-', counter = 0) : number {
+        if (!pancakes.includes('-')) {
+            return counter;
+        }
+        let index = 0;
+        let element = pancakes[index];
+
+        while(element && element !== str) {
+            pancakes[index] = str;
+            index++;
+            element = pancakes[index];
+        }
+        
+        counter++;
+        console.log(pancakes)
+        return this.flip(pancakes, str === '-' ? '+' : '-', counter);
+    }
 }
+
+// +-++
+// --++
+// ++++
--- 11 - RevengePancakes/tests/index.spec.ts
+++ 11 - RevengePancakes/tests/index.spec.ts
@@ -4,6 +4,24 @@ import RevengePancakes from '../src/index';
 describe('The Revenge of the pancakes', () => {
     const pancakes = new RevengePancakes();
 
-    it ('', () => {
+    it ('should flip 0 time when pancakes [+]', () => {
+        const res = pancakes.flip(['+']);
+        expect(res).to.equal(0);
     });
+    it ('should flip 1 when pancakes [-]', () => {
+        const res = pancakes.flip(['-']);
+        expect(res).to.equal(1);
+    });
+    // it ('should flip 1 when pancakes [-,+]', () => {
+    //     const res = pancakes.flip(['-', '+']);
+    //     expect(res).to.equal(1);
+    // });
+    // it ('should flip 2 when pancakes [+,-]', () => {
+    //     const res = pancakes.flip(['+', '-']);
+    //     expect(res).to.equal(2);
+    // });
+    // it ('should flip 3 when pancakes [-,-,+,-]', () => {
+    //     const res = pancakes.flip(['-', '-', '+', '-']);
+    //     expect(res).to.equal(3);
+    // });
 });
