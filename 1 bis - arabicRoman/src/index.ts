2b256ce1bae7c273dbd812801d9f46248643326d
ByJC <jeancharles.fauvin@gmail.com>
2019-02-13 17:00:17 +0100

    [ADD] Scaffolding arabicToRoman

---
+++ 1 bis - arabicRoman/src/index.ts
@@ -0,0 +1,5 @@
+export default class Converter {
+    // arabicToRoman(arabic) {
+    //     return "I";
+    // }
+}
---
+++ 1 bis - arabicRoman/tests/index.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import Converter from '../src/index';
+
+describe('arabicToRoman', () => {
+  const converter = new Converter();
+
+  // it('should return "I" when the value is 1', () => {
+  //   expect(converter.arabicToRoman(1)).to.equal("I");
+  // });
+});
