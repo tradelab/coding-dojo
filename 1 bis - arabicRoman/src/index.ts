97eb58af55feec0b7a6b3d4f5f101beb3c1da131
ByJC <jeancharles.fauvin@gmail.com>
2019-03-20 13:57:42 +0100

    [First coding dojo for people]

--- 1 bis - arabicRoman/src/index.ts
+++ 1 bis - arabicRoman/src/index.ts
@@ -1,5 +1,30 @@
+const recursiveGetResult = (conv, arabic, result) => {
+    Object.keys(conv)
+        .sort((a,b) => parseInt(b) - parseInt(a))
+        .forEach((key) => {
+            let intKey = parseInt(key);
+            if (arabic >= intKey) {
+                arabic -= intKey;
+                result += conv[intKey];
+            }
+        });
+        console.log(arabic);
+    if(arabic > 0) {
+        recursiveGetResult(conv, arabic, result);
+    } else {
+        return result;
+    }
+}
+
 export default class Converter {
-    // arabicToRoman(arabic) {
-    //     return "I";
-    // }
+    arabicToRoman(arabic: number): string {
+        const conv =  {
+            1: "I",
+            4: "IV",
+            5: "V",
+        };
+
+        const res =  recursiveGetResult(conv, arabic, "");
+        return res;
+    }
 }
--- 1 bis - arabicRoman/tests/index.spec.ts
+++ 1 bis - arabicRoman/tests/index.spec.ts
@@ -4,7 +4,23 @@ import Converter from '../src/index';
 describe('arabicToRoman', () => {
   const converter = new Converter();
 
-  // it('should return "I" when the value is 1', () => {
-  //   expect(converter.arabicToRoman(1)).to.equal("I");
-  // });
+  it('should return "I" when the value is 1', () => {
+    expect(converter.arabicToRoman(1)).to.equal("I");
+  });
+
+  it('should return "II" when the value is 2', () => {
+    expect(converter.arabicToRoman(2)).to.equal("II");
+  });
+
+  it('should return "IV" when the value is 4', () => {
+    expect(converter.arabicToRoman(4)).to.equal("IV");
+  });
+
+  it('should return "V" when the value is 5', () => {
+    expect(converter.arabicToRoman(5)).to.equal("V");
+  });
+
+  it('should return "VI" when the value is 6', () => {
+    expect(converter.arabicToRoman(6)).to.equal("VI");
+  });
 });
